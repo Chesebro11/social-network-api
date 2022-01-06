2ab7eb018aa565e3dd38c7527b2a832a3b371d3a
unknown <chesebrojared@gmail.com>
2022-01-06 08:33:45 -0800

    thought, reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            require: [true, 'You must provide text for the reaction'],
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: [true, 'You must provide a username.']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model, Types } = require('mongoose');
+// import date format when written
+
+const thoughtSchema = newSchema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You need to leave a thought!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
