4ca7c2a7c110a20bce5ab703b29eeabbe1ba0986
unknown <chesebrojared@gmail.com>
2022-01-06 16:05:10 -0800

    server runs, need to create data for walkthrough

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialapi', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
 });
 
 module.exports = mongoose.connection;
